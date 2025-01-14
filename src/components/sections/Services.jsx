b89a136b2c208c0854ef21d0f0b5b63d8f43a709
Abdullah Arafa <abdullahciti@gmx.de>
2025-01-14 10:18:34 +0100

    did the tailwindcss styles for services and added new imgs src

--- src/components/sections/Services.jsx
+++ src/components/sections/Services.jsx
@@ -1,3 +1,11 @@
+import { Link } from "react-router-dom";
+import SingelService from "./services/SingelService";
+import germanBeech from "../../../public/assets/images/woods/german-beech.png";
+import mahgoniWood from "../../../public/assets/images/woods/Mahgoni.png";
+import medepan from "../../../public/assets/images/woods/medepan-fr.jpg";
+import amricanSinian from "../../../public/assets/images/woods/Amirican sindian.png";
+import austrian from "../../../public/assets/images/woods/Austrian marbou3.png";
+import austrian25x5 from "../../../public/assets/images/woods/Austria 25x5.png";
 const Services = () => {
   return (
     <section
@@ -9,118 +17,64 @@ const Services = () => {
           <h2 className="text-3xl">Types of Wood We Offer</h2>
           <p>We Provide Superior Construction Services</p>
         </div>
-        <div className="service lg:col-span-1 bg-globalColor2 text-globalColor5  p-3 p-2 shadow-sm rounded-sm">
-          <div className="img p-1.5 overflow-hidden bg-white">
-            {" "}
-            <img
-              src="/public/assets/images/woods/Amirican sindian.png"
-              alt="Amirican-sindian"
-            />
-          </div>
-          <div className="text">
-            <h2>German beech wood</h2>
-            <p>
-              Beech wood has a very homogeneous surface, structure and hardness,
-              which means that the wood does not absorb the paint when
-              surface-treated, but the paint is distributed neatly and evenly on
-              the surface.
-            </p>
-            <a href="/home">Read More</a>
-          </div>
-        </div>
-        <div className="service lg:col-span-1 bg-globalColor2 text-globalColor5 p-3 ">
-          <div className="img p-1.5 overflow-hidden bg-white">
-            {" "}
-            <img
-              src="/public/assets/images/woods/Amirican sindian.png"
-              alt="Amirican-sindian"
-            />
-          </div>
-          <div className="text">
-            <h2>Mahogany wood</h2>
-            <p>
-              Mahogany is a straight-grained, reddish-brown hardwood from the
+        <SingelService
+          imgSrc={germanBeech}
+          imgAlt={"German beech wood"}
+          title={"German beech wood"}
+          desc={`Beech wood has a very homogeneous surface, structure and hardness,
+          which means that the wood does not absorb the paint when
+          surface-treated, but the paint is distributed neatly and evenly on the
+          surface.`}
+          sercivePath={"wood"}
+        />
+        <SingelService
+          imgSrc={mahgoniWood}
+          imgAlt={"Mahogany wood"}
+          title={"Mahogany wood"}
+          desc={`Mahogany is a straight-grained, reddish-brown hardwood from the
               tropical mahogany tree family. Mahogany is used commercially for a
-              variety of goods, due to its colorful and durable nature.
-            </p>
-            <a href="/home">Read More</a>
-          </div>
-        </div>
-        <div className="service lg:col-span-1 bg-globalColor2 text-globalColor5 p-3 ">
-          <div className="img p-1.5 overflow-hidden bg-white">
-            {" "}
-            <img
-              src="/public/assets/images/woods/Amirican sindian.png"
-              alt="Amirican-sindian"
-            />
-          </div>
-          <div className="text">
-            <h2>American Red Oak</h2>
-            <p>
-              Red oak is very strong, hard, heavy, dense, and has a very close
+              variety of goods, due to its colorful and durable nature.`}
+          sercivePath={"wood"}
+        />
+        <SingelService
+          imgSrc={medepan}
+          imgAlt={"Amirican-sindian"}
+          title={"French Medepen"}
+          desc={`French Medepen is very strong, hard, heavy, dense, and has a very close
               grain, making it highly resistant to insect and fungal damage. Oak
-              is commonly used in furniture, joinery, flooring, paneling, veneer
-            </p>
-            <a href="/home">Read More</a>
-          </div>
-        </div>
-        <div className="service lg:col-span-1 bg-globalColor2 text-globalColor5 p-3 ">
-          <div className="img p-1.5 overflow-hidden bg-white">
-            {" "}
-            <img
-              src="/public/assets/images/woods/Amirican sindian.png"
-              alt="Amirican-sindian"
-            />
-          </div>
-          <div className="text">
-            <h2>Swedish Wood</h2>
-            <p>
-              Beech wood has a very homogeneous surface, structure and hardness,
+              is commonly used in furniture, joinery, flooring, paneling, veneer`}
+          sercivePath={"wood-works"}
+        />
+        <SingelService
+          imgSrc={amricanSinian}
+          imgAlt={"Swedish wood"}
+          title={"Swedish Wood"}
+          desc={`Beech wood has a very homogeneous surface, structure and hardness,
               which means that the wood does not absorb the paint when
               surface-treated, but the paint is distributed neatly and evenly on
-              the surface.
-            </p>
-            <a href="/home">Read More</a>
-          </div>
-        </div>
-        <div className="service lg:col-span-1 bg-globalColor2 text-globalColor5 p-3 ">
-          <div className="img p-1.5 overflow-hidden bg-white">
-            {" "}
-            <img
-              src="/public/assets/images/woods/Amirican sindian.png"
-              alt="Amirican-sindian"
-            />
-          </div>
-          <div className="text">
-            <h2>Austrian</h2>
-            <p>
-              Beech wood has a very homogeneous surface, structure and hardness,
+              the surface.`}
+          sercivePath={"wood-works"}
+        />
+        <SingelService
+          imgSrc={austrian}
+          imgAlt={"Austrian"}
+          title={"Austrian"}
+          desc={`Beech wood has a very homogeneous surface, structure and hardness,
               which means that the wood does not absorb the paint when
               surface-treated, but the paint is distributed neatly and evenly on
-              the surface.
-            </p>
-            <a href="/home">Read More</a>
-          </div>
-        </div>
-        <div className="service lg:col-span-1 bg-globalColor2 text-globalColor5 p-3 ">
-          <div className="img p-1.5 overflow-hidden bg-white">
-            {" "}
-            <img
-              src="/public/assets/images/woods/Amirican sindian.png"
-              alt="Amirican-sindian"
-            />
-          </div>
-          <div className="text">
-            <h2>Austrian 25*5</h2>
-            <p>
-              Beech wood has a very homogeneous surface, structure and hardness,
+              the surface.`}
+          sercivePath={"wood-works"}
+        />
+        <SingelService
+          imgSrc={austrian25x5}
+          imgAlt={"Austrian 25*5 wood"}
+          title={"Austrian 25*5"}
+          desc={`Beech wood has a very homogeneous surface, structure and hardness,
               which means that the wood does not absorb the paint when
               surface-treated, but the paint is distributed neatly and evenly on
-              the surface.
-            </p>
-            <a href="/home">Read More</a>
-          </div>
-        </div>
+              the surface.`}
+          sercivePath={"wood-works"}
+        />
       </div>
     </section>
   );
