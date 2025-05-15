fb34e1eda810654b6563a92f97a2afe0464f21b5
Abdullah <136592513+Abdullahciti@users.noreply.github.com>
2025-05-15 13:03:35 +0200

    Update AboutLeft.jsx

--- src/components/sections/about/AboutLeft.jsx
+++ src/components/sections/about/AboutLeft.jsx
@@ -80,7 +80,7 @@ const AboutLeft = () => {
       <img src={arMap} alt="starwood map" className="max-w-full max-h-full" />
       {/* Jeddah */}
       <div className="absolute animate-bounce top-[62%] left-[22%] flex flex-col">
-        <a href="https://maps.app.goo.gl/UdCniY4RYarYocEfA" target="_blank">
+        <a href="https://maps.app.goo.gl/4wcrFf2DLzqg3LFL9?g_st=com.google.maps.preview.copy" target="_blank">
           <img
             src={starwoodStar}
             alt="starwood marker"
